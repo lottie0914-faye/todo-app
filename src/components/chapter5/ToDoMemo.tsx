2436b4a7a51e0f71852709530e392fbc09e26f06
yufeifei.li <yufeifei.li@kddi-web.com>
2022-06-10 14:34:43 +0900

    eslint installed

--- src/components/chapter5/ToDoMemo.tsx
+++ src/components/chapter5/ToDoMemo.tsx
@@ -1,22 +1,22 @@
-import { Memo } from '../../types/Memo';
 import { Button } from '@mui/material';
-import { TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineSeparator } from '@mui/lab';
+import {
+  TimelineConnector, TimelineContent, TimelineDot, TimelineItem, TimelineSeparator,
+} from '@mui/lab';
+import { Task } from '../../types/Task';
 
-export const ToDoMemo = (props: Memo) => {
-    const { title, removeToDo, timeStamp } = props;
-    return (
-        <>
-            <TimelineItem>
-                <TimelineSeparator>
-                    <TimelineDot />
-                    <TimelineConnector />
-                </TimelineSeparator>
-                <TimelineContent>
-                    <p>{timeStamp}</p>
-                    <p>{ title }</p>
-                    <Button variant="text" onClick={removeToDo} >x</Button>
-                </TimelineContent>
-            </TimelineItem>
-        </>
-    );
-};
+export const ToDoMemo: JSX.IntrinsicAttributes(props: Task): JSX.Element => {
+  const { title, removeToDo, timeStamp } = props;
+  return (
+    <TimelineItem>
+      <TimelineSeparator>
+        <TimelineDot />
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>
+        <p>{timeStamp}</p>
+        <p>{ title }</p>
+        <Button variant="text" onClick={removeToDo}>x</Button>
+      </TimelineContent>
+    </TimelineItem>
+  );
+}
